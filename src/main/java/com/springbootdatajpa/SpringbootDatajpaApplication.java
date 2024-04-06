4ff3cbf4ae0dd240677ba59ccf1cc983ea9fd2f3
skushwaha59 <topgun@localhost.localdomain>
2024-04-07 02:48:52 +0530

    initial commit

---
+++ src/main/java/com/springbootdatajpa/SpringbootDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatajpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdatajpa/SpringbootDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
